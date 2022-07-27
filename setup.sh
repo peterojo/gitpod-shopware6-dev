6bac2cc9386aae8c36e6a3e7e3dc3320a93f5b9a
peterojo <peter.ojo@adyen.com>
2022-07-27 14:42:32 +0200

    install adyen plugin

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@
 docker exec shopware mysql -u root --password='root' shopware -e "update sales_channel_domain set url='${GP_URL}' where url LIKE '%localhost%'" 2> /dev/null
 docker exec shopware mysql -u root --password='root' shopware_test -e "update sales_channel_domain set url='${GP_URL}' where url LIKE '%localhost%'" 2> /dev/null
 docker exec shopware mysql -u root --password='root' shopware_e2e -e "update sales_channel_domain set url='${GP_URL}' where url LIKE '%localhost%'" 2> /dev/null
+
+docker exec shopware composer require adyen/adyen-shopware6:dev-develop
