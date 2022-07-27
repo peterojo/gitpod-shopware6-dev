40e93c8eabfcb65c6cc3cc85d591062ebb24014c
Peter Ojo <peter-ojo@hotmail.com>
2022-07-27 14:02:38 +0000

    move gp url constant to sh file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export GP_URL=$(gp url)
+
 docker exec shopware mysql -u root --password='root' shopware -e "update sales_channel_domain set url='${GP_URL}' where url LIKE '%localhost%'" 2> /dev/null
 docker exec shopware mysql -u root --password='root' shopware_test -e "update sales_channel_domain set url='${GP_URL}' where url LIKE '%localhost%'" 2> /dev/null
 docker exec shopware mysql -u root --password='root' shopware_e2e -e "update sales_channel_domain set url='${GP_URL}' where url LIKE '%localhost%'" 2> /dev/null
